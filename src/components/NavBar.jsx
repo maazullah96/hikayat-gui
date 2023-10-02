f41716f8753a06f87a39d6b1c634064c94dc55f1
maazullah96 <maazullahminhas96@gmail.com>
2023-10-02 23:23:46 +0500

    React-Router-Added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,148 @@
+// import hikayatImage from '../assets/hikayat-03.png'
+// import { useState } from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import {
+//   faBars,
+//   faToggleOn,
+//   faToggleOff,
+//   faBookOpen,
+//   faList,
+//   faTag,
+//   faChevronUp,
+//   faChevronDown,
+//   // faStar,
+//   faGear
+// } from '@fortawesome/free-solid-svg-icons'
+// import Container from 'react-bootstrap/Container'
+// import Navbar from 'react-bootstrap/Navbar'
+// import { Offcanvas, Stack, ListGroup, Dropdown } from 'react-bootstrap'
+// function NavBar() {
+//   const [show, setShow] = useState(false)
+//   const [theme, setTheme] = useState('light')
+//   const handleClose = () => setShow(false)
+//   const toggleShow = () => setShow((s) => !s)
+//   const toggleTheme = () => setTheme((t) => (t === 'light' ? 'dark' : 'light'))
+
+//   const [showBooksDropdown, setShowBooksDropdown] = useState(false)
+//   const [showCategoriesDropdown, setShowCategoriesDropdown] = useState(false)
+
+//   const toggleBooksDropdown = () => {
+//     setShowBooksDropdown(!showBooksDropdown)
+//     setShowCategoriesDropdown(false)
+//   }
+
+//   const toggleCategoriesDropdown = () => {
+//     setShowCategoriesDropdown(!showCategoriesDropdown)
+//     setShowBooksDropdown(false)
+//   }
+
+//   return (
+//     <>
+//       <>
+//         <Offcanvas
+//           show={show}
+//           onHide={handleClose}
+//           scroll={true}
+//           backdrop={true}
+//         >
+//           <Offcanvas.Header>
+//             <Offcanvas.Title>
+//               <img src={hikayatImage} alt='Hikayat Logo' />
+//             </Offcanvas.Title>
+//           </Offcanvas.Header>
+//           <Offcanvas.Body style={{ display: 'flex', flexDirection: 'column' }}>
+//             <ListGroup variant='flush'>
+//               <ListGroup.Item onClick={toggleBooksDropdown}>
+//                 <FontAwesomeIcon icon={faBookOpen} /> {' Books'}
+//                 {showBooksDropdown ? (
+//                   <FontAwesomeIcon icon={faChevronUp} />
+//                 ) : (
+//                   <FontAwesomeIcon icon={faChevronDown} />
+//                 )}
+//                 {showBooksDropdown && (
+//                   <div className='dropdown-content'>
+//                     {/* Add your dynamic content for Books here */}
+//                     <p>Book 1</p>
+//                     <p>Book 2</p>
+//                     <p>Book 3</p>
+//                   </div>
+//                 )}
+//               </ListGroup.Item>
+//               <ListGroup.Item onClick={toggleCategoriesDropdown}>
+//                 <FontAwesomeIcon icon={faList} /> {' Categories'}
+//                 {showCategoriesDropdown ? (
+//                   <FontAwesomeIcon icon={faChevronUp} />
+//                 ) : (
+//                   <FontAwesomeIcon icon={faChevronDown} />
+//                 )}
+//                 {showCategoriesDropdown && (
+//                   <div className='dropdown-content'>
+//                     {/* Add your dynamic content for Categories here */}
+//                     <p>Category 1</p>
+//                     <p>Category 2</p>
+//                     <p>Category 3</p>
+//                   </div>
+//                 )}
+//               </ListGroup.Item>
+//               <hr /> {/* Add a horizontal line after the second list item */}
+//               <ListGroup.Item>
+//                 <FontAwesomeIcon icon={faTag} rotation={90} color='black' />{' '}
+//                 {'  keyword'}
+//               </ListGroup.Item>
+//               <ListGroup.Item>
+//                 <FontAwesomeIcon icon={faTag} rotation={90} color='black' />{' '}
+//                 {'  keyword'}
+//               </ListGroup.Item>
+//               <ListGroup.Item>
+//                 <FontAwesomeIcon icon={faTag} rotation={90} color='black' />{' '}
+//                 {'  keyword'}
+//               </ListGroup.Item>
+//               <hr />
+//               <ListGroup.Item style={{ marginTop: 'auto' }}>
+//                 <FontAwesomeIcon
+//                   icon={faGear}
+//                   rotation={90}
+//                   color='black'
+//                   size='1x'
+//                 />{' '}
+//                 {'  Settings'}
+//               </ListGroup.Item>
+//             </ListGroup>
+//           </Offcanvas.Body>
+//         </Offcanvas>
+//       </>
+//       <Navbar className='mt-2' style={{ border: '1px solid black' }}>
+//         <Container
+//           fluid
+//           className='d-flex justify-content-between align-items-center'
+//         >
+//           <FontAwesomeIcon
+//             onClick={toggleShow}
+//             icon={faBars}
+//             size='xl'
+//             style={{ cursor: 'pointer', marginLeft: '10px' }}
+//           />
+//           <Navbar.Brand
+//             href='#'
+//             id='main-title'
+//             style={{ fontFamily: 'Nevan', fontSize: '28px', color: '#AF985A' }}
+//           >
+//             HIKAYAT
+//           </Navbar.Brand>
+//           <FontAwesomeIcon
+//             onClick={toggleTheme}
+//             size='2xl'
+//             icon={theme === 'light' ? faToggleOn : faToggleOff}
+//             style={{ cursor: 'pointer', marginRight: '10px' }}
+//           />
+//         </Container>
+//       </Navbar>
+//     </>
+//   )
+// }
+
+// export default NavBar
+
 import hikayatImage from '../assets/hikayat-03.png'
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -8,62 +153,112 @@ import {
   faBookOpen,
   faList,
   faTag,
-  faStar
+  faChevronUp,
+  faChevronDown,
+  faGear
 } from '@fortawesome/free-solid-svg-icons'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
-import Offcanvas from 'react-bootstrap/Offcanvas'
-import { ListGroupItem, ListGroup } from 'react-bootstrap'
-import { Stack } from 'react-bootstrap'
+import { Offcanvas, Stack, ListGroup, Dropdown } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   const [show, setShow] = useState(false)
   const [theme, setTheme] = useState('light')
-
   const handleClose = () => setShow(false)
   const toggleShow = () => setShow((s) => !s)
   const toggleTheme = () => setTheme((t) => (t === 'light' ? 'dark' : 'light'))
 
+  const [showBooksDropdown, setShowBooksDropdown] = useState(false)
+  const [showCategoriesDropdown, setShowCategoriesDropdown] = useState(false)
+
+  const toggleBooksDropdown = () => {
+    setShowBooksDropdown(!showBooksDropdown)
+    setShowCategoriesDropdown(false)
+  }
+
+  const toggleCategoriesDropdown = () => {
+    setShowCategoriesDropdown(!showCategoriesDropdown)
+    setShowBooksDropdown(false)
+  }
+  const books = ['Maktub', 'AlChemist', 'Rich Dad Poor Dad']
+  const categories = ['Pyar', 'Crime', 'THriller', 'Dead End']
   return (
     <>
-      <Offcanvas show={show} onHide={handleClose} scroll={true} backdrop={true}>
-        <Offcanvas.Header>
-          <Offcanvas.Title>
-            <img src={hikayatImage} />
-          </Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Stack gap={1}>
+      <>
+        <Offcanvas
+          show={show}
+          onHide={handleClose}
+          scroll={true}
+          backdrop={true}
+        >
+          <Offcanvas.Header>
+            <Offcanvas.Title>
+              <img src={hikayatImage} alt='Hikayat Logo' />
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body style={{ display: 'flex', flexDirection: 'column' }}>
             <ListGroup variant='flush'>
-              <ListGroup.Item>
+              <ListGroup.Item onClick={toggleBooksDropdown}>
                 <FontAwesomeIcon icon={faBookOpen} /> {' Books'}
+                {showBooksDropdown ? (
+                  <FontAwesomeIcon icon={faChevronUp} />
+                ) : (
+                  <FontAwesomeIcon icon={faChevronDown} />
+                )}
+                {showBooksDropdown && (
+                  <div className='dropdown-content-right'>
+                    {books.map((book, index) => (
+                      <Link to={`book/${index}`} key={index}>
+                        {book}
+                      </Link>
+                      // <p key={index}>{book}</p>
+                    ))}
+                  </div>
+                )}
               </ListGroup.Item>
-              <ListGroup.Item>
+              <ListGroup.Item onClick={toggleCategoriesDropdown}>
                 <FontAwesomeIcon icon={faList} /> {' Categories'}
+                {showCategoriesDropdown ? (
+                  <FontAwesomeIcon icon={faChevronUp} />
+                ) : (
+                  <FontAwesomeIcon icon={faChevronDown} />
+                )}
+                {showCategoriesDropdown && (
+                  <div className='dropdown-content-right'>
+                    {categories.map((category, index) => (
+                      <p key={index}>{category}</p>
+                    ))}
+                  </div>
+                )}
               </ListGroup.Item>
-              <hr /> {/* Add horizontal line after second list item */}
+              <hr /> {/* Add a horizontal line after the second list item */}
               <ListGroup.Item>
-                <FontAwesomeIcon icon={faTag} rotation={90} color='black' />
-                {'   ' + 'keyword'}
+                <FontAwesomeIcon icon={faTag} rotation={90} color='black' />{' '}
+                {'  keyword'}
               </ListGroup.Item>
               <ListGroup.Item>
-                <FontAwesomeIcon icon={faTag} rotation={90} color='black' />
-                {'   ' + 'keyword'}
+                <FontAwesomeIcon icon={faTag} rotation={90} color='black' />{' '}
+                {'  keyword'}
               </ListGroup.Item>
               <ListGroup.Item>
-                <FontAwesomeIcon icon={faTag} rotation={90} color='black' />
-                {'   ' + 'keyword'}
+                <FontAwesomeIcon icon={faTag} rotation={90} color='black' />{' '}
+                {'  keyword'}
               </ListGroup.Item>
               <hr />
-              <ListGroup.Item>
-                <FontAwesomeIcon icon={faStar} />
-                {' ' + 'Rate the App'}
+              <ListGroup.Item style={{ marginTop: 'auto' }}>
+                <FontAwesomeIcon
+                  icon={faGear}
+                  rotation={90}
+                  color='black'
+                  size='1x'
+                />{' '}
+                {'  Settings'}
               </ListGroup.Item>
             </ListGroup>
-          </Stack>
-        </Offcanvas.Body>
-      </Offcanvas>
-
+          </Offcanvas.Body>
+        </Offcanvas>
+      </>
       <Navbar className='mt-2' style={{ border: '1px solid black' }}>
         <Container
           fluid
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NotFound from './components/NotFound.jsx'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from 'react-router-dom'
+
+{
+  /* <Route index element={<Listings />}></Route> */
+}
+{
+  /* <Route path='listing'> */
+}
+{
+  /* <Route path=':listingId' element={<SingeListingPage />}></Route> */
+}
+{
+  /* </Route> */
+}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route component={NotFound} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 )
